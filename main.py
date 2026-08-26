565b92723539625b8051d7a7768bdbd8b871df31
Muhammadyasinafridi <66191515+Muhammadyasinafridi@users.noreply.github.com>
2026-08-26 22:34:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -128,4 +128,4 @@ def LeadSegmentor(features:LeadsFeature):
 #To run 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("Main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
